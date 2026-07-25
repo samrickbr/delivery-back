d13d940be457810a6bc2facff1f20567b638abe9
Rick <ricrdocardoso@gmail.com>
2026-07-25 00:42:22 -0300

    Libera CORS para frontend publicado

--- src/main/java/br/com/inova/sigin/delivery/config/CorsConfig.java
+++ src/main/java/br/com/inova/sigin/delivery/config/CorsConfig.java
@@ -18,7 +18,8 @@ public class CorsConfig {
 
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "http://localhost:5173"
+                                "http://localhost:5173",
+                                "https://delivery-front-tawny.vercel.app"
                         )
                         .allowedMethods(
                                 "GET",
