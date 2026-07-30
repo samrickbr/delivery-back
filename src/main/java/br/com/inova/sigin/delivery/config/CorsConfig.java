856682529e9c1a984677494c3a7bd7442c7f7daf
Rick <ricrdocardoso@gmail.com>
2026-07-30 11:02:44 -0300

    fix(cors): permite acessos do Vercel via allowedOriginPatterns

--- src/main/java/br/com/inova/sigin/delivery/config/CorsConfig.java
+++ src/main/java/br/com/inova/sigin/delivery/config/CorsConfig.java
@@ -17,9 +17,9 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:5173",
-                                "https:*.vercel.app"
+                        .allowedOriginPatterns(
+                                "http://localhost:*",
+                                "https://*.vercel.app"
                         )
                         .allowedMethods(
                                 "GET",
